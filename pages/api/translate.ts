93d68470243932f15576aa48cc4001d7d36d3633
Rida F'kih <hello@rida.dev>
2022-11-17 02:57:53 -0700

    add two-step translation API request with DeepL

---
+++ pages/api/translate.ts
@@ -0,0 +1,47 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { Translator } from "deepl-node";
+import { z } from "zod";
+
+export type TranslateResponseSchema = {
+  original: string;
+  candidate: string;
+  complete: string;
+};
+
+export const TranslateRequestSchema = z.object({
+  prospect: z.string(),
+});
+
+const confirmRequestBody = async (body: unknown) => {
+  const response = await TranslateRequestSchema.safeParse(body);
+
+  if (response.success) return response.data;
+};
+
+const handler = async (
+  req: NextApiRequest,
+  res: NextApiResponse<TranslateResponseSchema>
+) => {
+  const translator = new Translator(process.env.DEEPL_AUTH_KEY!);
+
+  const body = await confirmRequestBody(req.body);
+  if (!body) return res.status(400).end();
+
+  const { prospect: original } = body;
+
+  const { text: candidate } = await translator.translateText(original, "fr", "en-US", {
+    formality: "prefer_less",
+  });
+
+  const { text: complete } = await translator.translateText(candidate, "en", "fr", {
+    formality: "prefer_less",
+  });
+
+  res.status(200).json({
+    original,
+    candidate,
+    complete,
+  });
+};
+
+export default handler;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,28 +1,70 @@
+import axios, { AxiosResponse } from "axios";
 import { useState } from "react";
+import type {
+  TranslateResponseSchema,
+  TranslateRequestSchema,
+} from "./api/translate";
+import type { z } from "zod";
+import { ArrowDownIcon, InfoCircledIcon } from "@radix-ui/react-icons";
 
 export default function Home() {
   const [translationText, setTranslationText] = useState<string>("");
+  const [completed, setCompleted] = useState<TranslateResponseSchema | null>();
 
-  const handleClick = () => {};
+  const handleClick = () => {
+    axios
+      .post<
+        null,
+        AxiosResponse<TranslateResponseSchema>,
+        z.infer<typeof TranslateRequestSchema>
+      >("/api/translate", {
+        prospect: translationText,
+      })
+      .then(({ data }) => setCompleted(data));
+  };
 
   return (
-    <div className="w-full h-full flex flex-col items-center justify-center gap-4 bg-black p-4">
-      <h1 className="text-xl font-bold text-white">Back&apos;n&apos;forth Translator</h1>
-      <div className="flex gap-2">
-        <input
+    <div className="w-full h-full flex flex-col items-center justify-center gap-8 bg-neutral-900 p-4">
+      <h1 className="text-xl font-bold text-white">
+        French Naturalizer
+      </h1>
+      <div className="flex flex-col gap-2 w-full max-w-xl">
+        <textarea
           value={translationText}
           onChange={({ target }) => setTranslationText(target.value)}
-          type="text"
           placeholder="Enter a phrase to translate."
-          className="bg-neutral-900 px-2 py-1 rounded-sm border border-neutral-700 placeholder:text-neutral-600 min-w-[16rem] text-white focus:outline-none focus:border-neutral-600"
+          className="block w-full p-4 bg-neutral-800 rounded-sm border-neutral-700 border text-neutral-100 outline-none focus:border-neutral-600"
         />
         <button
+          disabled={translationText.length <= 0}
           onClick={handleClick}
-          className="bg-blue-600 px-4 text-white rounded-sm border border-blue-500 hover:border-blue-400 hover:bg-blue-500 active:border-blue-600 active:bg-blue-700 transition-colors"
+          className="bg-blue-600 px-4 py-1.5 w-full text-white rounded-sm border border-blue-500 hover:border-blue-400 hover:bg-blue-500 active:border-blue-600 active:bg-blue-700 transition-all disabled:opacity-40 disabled:filter disabled:saturate-0"
         >
-          Translate
+          Naturalize
         </button>
       </div>
+        {completed && (
+          <div className="text-white flex flex-col items-center gap-4 text-center">
+            <div className="p-4 bg-neutral-800 rounded-md border-neutral-700 border">
+              <h2 className="uppercase text-xs text-neutral-400">your version</h2>
+              <p className="max-w-[48ch] leading-7 text-neutral-200">{completed.original}</p>
+            </div>
+            <div className="bg-blue-500 p-2 rounded-full">
+              <ArrowDownIcon />
+            </div>
+            <div className="flex flex-col p-4 bg-neutral-800 rounded-md border-neutral-700 border relative">
+              <h2 className="uppercase text-xs text-neutral-400">more natural</h2>
+              <p className="max-w-[48ch] leading-7 text-neutral-200">{completed.complete}</p>
+              <div className="bg-neutral-700 border-neutral-500 border p-1 rounded-full w-fit absolute -bottom-4 right-6 hover:bg-neutral-600 hover:border-neutral-400 hover:cursor-pointer group">
+                <div className="min-w-[32rem] p-4 hidden group-hover:absolute group-hover:block -bottom-2 right-0 transform translate-y-full rounded-sm shadow-lg bg-neutral-800 border-neutral-700 border">
+                  <h2 className="uppercase text-xs text-neutral-400">...which means</h2>
+                  <p className="text-xs leading-6 text-neutral-200 mt-2">{completed.candidate}</p>
+                </div>
+                <InfoCircledIcon color="white" />
+              </div>
+            </div>
+          </div>
+        )}
     </div>
   );
 }
